85a702ac38f6e05334010e0be81392338003d497
ootakaito <electronicswork03@gmail.com>
2024-06-04 06:31:17 +0900

    button change

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const handleClick = async () => {
 return (
     <div className={styles.page}>
         <button onClick={handleClick} className={styles.button}>
-            他のにゃんこも見る
+            One more cat!
             </button>
         <div className={styles.frame}>
             {loading || <img src={imageUrl} className={styles.img} />}
